3c49543ed719a7ad7346ef9248b9a2aebd7a0b71
admin <admin@admins-MacBook-Pro.local>
2025-07-08 15:02:07 +0530

    latest code

--- lib/view/intro/components/download_button.dart
+++ lib/view/intro/components/download_button.dart
@@ -10,7 +10,7 @@ class DownloadButton extends StatelessWidget {
     return InkWell(
       onTap: () {
         launchUrl(Uri.parse(
-            'https://drive.google.com/file/d/1fFwsSoQoE8ysnWLQHmgyGiZAIeYoydWf/view'));
+            'https://drive.google.com/file/d/1_Z3pvL9nL-K-UeV0G6FW_LNT_NTlVgk1/view'));
       },
       child: Container(
         alignment: Alignment.center,
