b39fda9e9ea0983ab2dfc7bf25d421078d649749
admin <admin@admins-MacBook-Pro.local>
2025-07-08 15:08:00 +0530

    latest code

--- install_flutter.sh
+++ install_flutter.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-# Install Flutter in Netlify CI
 
+# Download Flutter
 git clone https://github.com/flutter/flutter.git -b stable
+
+# Export Flutter path
 export PATH="$PATH:`pwd`/flutter/bin"
+
+# Run Flutter doctor to trigger download of dependencies
 flutter doctor
+
+# Enable web support
+flutter config --enable-web
+
+# Get dependencies
+flutter pub get
